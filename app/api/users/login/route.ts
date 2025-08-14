ace26a851e90e2543a306b6aefcf5e93c6b4c402
Puffssss1 <budjiangelo@gmail.com>
2025-08-14 18:22:51 +0800

    deployment fix

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -1,45 +1,45 @@
-import { supabase } from "@/lib/supabase";
-import { NextResponse } from "next/server";
+// import { supabase } from "@/lib/supabase";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-  const { data, error } = await supabase
-    .from("user_profile")
-    .select("id, email, name");
-  if (error) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
-  }
-  return NextResponse.json(data);
-}
+// export async function GET() {
+//   const { data, error } = await supabase
+//     .from("user_profile")
+//     .select("id, email, name");
+//   if (error) {
+//     return NextResponse.json({ message: error.message }, { status: 400 });
+//   }
+//   return NextResponse.json(data);
+// }
 
-export async function POST(req: Request) {
-  const body = await req.json();
-  const { name, email, password } = body;
+// export async function POST(req: Request) {
+//   const body = await req.json();
+//   const { name, email, password } = body;
 
-  const data = {
-    name: name,
-    email: email,
-    password: password,
-  };
+//   const data = {
+//     name: name,
+//     email: email,
+//     password: password,
+//   };
 
-  const { error } = await supabase.auth.signUp(data);
-  const user = await supabase.from("user_profile").insert({
-    email: data.email,
-    name: data.name,
-  });
+//   const { error } = await supabase.auth.signUp(data);
+//   const user = await supabase.from("user_profile").insert({
+//     email: data.email,
+//     name: data.name,
+//   });
 
-  // if(insertError) {
-  //   return {
-  //     status: insertError.message
-  //   }
-  // }
-  console.log(user);
+//   // if(insertError) {
+//   //   return {
+//   //     status: insertError.message
+//   //   }
+//   // }
+//   console.log(user);
 
-  // const { error } = await supabase.auth.signInWithPassword(data);
+//   // const { error } = await supabase.auth.signInWithPassword(data);
 
-  if (error) {
-    console.log(error);
-    return NextResponse.json({ message: error.message });
-  }
+//   if (error) {
+//     console.log(error);
+//     return NextResponse.json({ message: error.message });
+//   }
 
-  return NextResponse.json({ message: "login success" });
-}
+//   return NextResponse.json({ message: "login success" });
+// }
--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,63 +1,63 @@
-import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
-import * as argon2 from "argon2";
-import { supabase } from "@/lib/supabase";
+// import { NextResponse } from "next/server";
+// import { v4 as uuidv4 } from "uuid";
+// import * as argon2 from "argon2";
+// import { supabase } from "@/lib/supabase";
 
-export async function GET() {
-  const { data, error } = await supabase
-    .from("users")
-    .select("created_at, id, email, name");
-  if (error) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
-  }
-  return NextResponse.json(data);
-}
+// export async function GET() {
+//   const { data, error } = await supabase
+//     .from("users")
+//     .select("created_at, id, email, name");
+//   if (error) {
+//     return NextResponse.json({ message: error.message }, { status: 400 });
+//   }
+//   return NextResponse.json(data);
+// }
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { name, email, password } = body;
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
+//     const { name, email, password } = body;
 
-    if (!name || !email || !password) {
-      return NextResponse.json(
-        { error: "Name, email, and password are required." },
-        { status: 400 }
-      );
-    }
+//     if (!name || !email || !password) {
+//       return NextResponse.json(
+//         { error: "Name, email, and password are required." },
+//         { status: 400 }
+//       );
+//     }
 
-    // Check if email already exists
-    const { data: userExist } = await supabase
-      .from("users")
-      .select("*")
-      .eq("email", email)
-      .single();
+//     // Check if email already exists
+//     const { data: userExist } = await supabase
+//       .from("users")
+//       .select("*")
+//       .eq("email", email)
+//       .single();
 
-    if (userExist) {
-      return NextResponse.json(
-        { error: "Email already registered." },
-        { status: 409 }
-      );
-    }
+//     if (userExist) {
+//       return NextResponse.json(
+//         { error: "Email already registered." },
+//         { status: 409 }
+//       );
+//     }
 
-    const hashed = await argon2.hash(password);
+//     const hashed = await argon2.hash(password);
 
-    const newUser = {
-      id: uuidv4(),
-      name,
-      email,
-      password: hashed,
-    };
-    const { error } = await supabase.from("users").insert([newUser]);
-    if (error) {
-      console.log(error);
-      return NextResponse.json({ message: error.message }, { status: 400 });
-    }
-    return NextResponse.json({ message: "sign up success!" }, { status: 200 });
-  } catch (error) {
-    console.error("POST error:", error);
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
-  }
-}
+//     const newUser = {
+//       id: uuidv4(),
+//       name,
+//       email,
+//       password: hashed,
+//     };
+//     const { error } = await supabase.from("users").insert([newUser]);
+//     if (error) {
+//       console.log(error);
+//       return NextResponse.json({ message: error.message }, { status: 400 });
+//     }
+//     return NextResponse.json({ message: "sign up success!" }, { status: 200 });
+//   } catch (error) {
+//     console.error("POST error:", error);
+//     return NextResponse.json(
+//       { error: "Something went wrong" },
+//       { status: 500 }
+//     );
+//   }
+// }
